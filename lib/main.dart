685baefe76c15f7a8d824fa80dd3b2eb64a407d3
TonnyKamau <tonnykamau6@gmail.com>
2022-10-31 03:34:39 +0300

    router

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/models/error_model.dart';
+import 'package:google_docs_clone/repository/auth_repository.dart';
+import 'package:google_docs_clone/router.dart';
+import 'package:google_docs_clone/screens/homescreen.dart';
 import 'package:google_docs_clone/screens/login.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  ErrorModel? errorModel;
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() async {
+    errorModel = await ref.read(authRepositoryProvider).getUserData();
+    if (errorModel != null && errorModel!.data != null) {
+      ref.read(userProvider.notifier).update((state) => errorModel!.data);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Google Docs',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Login(),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+        final user = ref.watch(userProvider);
+        if (user != null && user.token.isNotEmpty) {
+          return loggedInRoute;
+        }
+        return loggedOutRoute;
+      }),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/colors.dart';
 import 'package:google_docs_clone/repository/auth_repository.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+  void signOut(WidgetRef ref) {
+    ref.read(authRepositoryProvider).signOut();
+    ref.read(userProvider.notifier).update((state) => null);
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: CWhitecolor, elevation: 0, actions: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+              color: CBlackcolor,
+            )),
+        IconButton(
+            onPressed: () =>signOut(ref),
+            icon: const Icon(
+              Icons.logout,
+              color: CRedcolor,
+            )),
+      ]),
       body: Center(
         child: Text(
           ref.watch(userProvider)!.email,
