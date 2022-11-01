5c079e5ff0605df858e2acfb11746c8b39936f2b
TonnyKamau <tonnykamau6@gmail.com>
2022-11-01 16:42:22 +0300

    document

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
+const documentRouter = require("./routes/document")
 const app = express();
 const cors =require('cors');
 
 app.use(cors());
 app.use(express.json());
 app.use(authRouter);
+app.use(documentRouter);
 const PORT = process.env.PORT | 3001;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`app connected at ${PORT}`);
